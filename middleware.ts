d3e023ffc065d52bafa7ba4e2ef2d7d55636c393
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-04-24 17:55:51 +0100

    ...

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/demo"],
+  publicRoutes: ["/", "/demo","/faqs"],
 });
 
 export const config = {
