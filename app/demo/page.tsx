8418145badcfb09e862e4ce631a9fd8525d57548
Verifieddanny <dannyclassic56@gmail.com>
2024-04-27 12:46:04 +0100

    Fix[page size]

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -5,7 +5,7 @@ import EventTemplate from "../_ui/EventTemplate";
 
 export default function page() {
   return (
-    <div className="">
+    <div className="w-full h-full bg-defaultBackground mx-auto flex justify-center items-center">
       <EventTemplate />
     </div>
   );
