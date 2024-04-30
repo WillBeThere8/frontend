174ca097fee4f3eda0fcfef14e1bf993f2755522
Unclephil <philipnssien@gmail.com>
2024-04-30 21:23:53 +0100

    clerk redirect

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <ClerkProvider>
+    <ClerkProvider>
+      <html lang="en">
         <body className={`${poppins.className}`}>{children}</body>
-      </ClerkProvider>
-      <Toaster />
-    </html>
+        <Toaster />
+      </html>
+    </ClerkProvider>
   );
 }
