d0453c5095d61716f25c1fbcdead71b765f23769
Verifieddanny <dannyclassic56@gmail.com>
2024-04-30 15:34:30 +0100

    Fix[mobile view of invite page]

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/hello", "/contact", "/faqs", "/about", /^\/invite\/.*/],
+  publicRoutes: [
+    "/",
+    "/hello",
+    "/contact",
+    "/faqs",
+    "/about",
+    /^\/invite\/.*/,
+    "/sign-in",
+  ],
 });
 
 export const config = {
