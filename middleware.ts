e25cda7db712e1d08202c3fbe6bbd4409e6d4b37
Verifieddanny <dannyclassic56@gmail.com>
2024-07-15 17:37:39 +0100

    Demo added

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-<<<<<<< HEAD
   publicRoutes: ["/", "/hello", "/contact", "/faqs", "/demo" , "/about", /^\/invite\/.*/],
-=======
-  publicRoutes: ["/", "/contact", "/faqs", "/about", /^\/invite\/.*/],
->>>>>>> 174ca097fee4f3eda0fcfef14e1bf993f2755522
 });
 
 export const config = {
